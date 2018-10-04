8c9dfa88a02dba700e21893e0b4277c5b5b17d4d
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-10-05 00:46:20 +0900

    Slak通知文を変更

--- app/controllers/attends_controller.rb
+++ app/controllers/attends_controller.rb
@@ -3,7 +3,7 @@ class AttendsController < ApplicationController
     @attend = Attend.create(attend_params)
     if Rails.env == 'production'
       notifier = Slack::Notifier.new(Rails.application.config.slack_webhook_url)
-      message = @attend.user.user_name.to_s + "さんが" + @attend.event.name.to_s + "の参加を表明しました。現在のパーティ参加者は" + @attend.event.attends.size.to_s + "人です。"
+      message = @attend.user.user_name.to_s + "さんが「" + @attend.event.name.to_s + "」の参加を表明しました。現在のパーティ参加者は" + @attend.event.attends.size.to_s + "人です。"
       notifier.ping(message)
     end
   end
@@ -13,7 +13,7 @@ class AttendsController < ApplicationController
     if Rails.env == 'production'
       notifier = Slack::Notifier.new(Rails.application.config.slack_webhook_url)
       @numbers = @attend.event.attends.size - 1 
-      message = @attend.user.user_name.to_s + "さんが" + @attend.event.name + "の参加をキャンセルしました。現在のパーティ参加者は" + @numbers.to_s + "人です。"
+      message = @attend.user.user_name.to_s + "さんが「" + @attend.event.name + "」の参加をキャンセルしました。現在のパーティ参加者は" + @numbers.to_s + "人です。"
       notifier.ping(message)
     end
     @attend.destroy
