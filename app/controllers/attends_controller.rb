172db02d1d5724a0e222507efb344a29efcc5ba8
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-10-05 01:19:24 +0900

    herokuだとslackの通知が動かない?

--- app/controllers/attends_controller.rb
+++ app/controllers/attends_controller.rb
@@ -1,21 +1,21 @@
 class AttendsController < ApplicationController
   def create
     @attend = Attend.create(attend_params)
-    if Rails.env == 'production'
+    #if Rails.env == 'production'
       notifier = Slack::Notifier.new(Rails.application.config.slack_webhook_url)
       message = @attend.user.user_name.to_s + "さんが「" + @attend.event.name.to_s + "」の参加を表明しました。現在のパーティ参加者は" + @attend.event.attends.size.to_s + "人です。"
       notifier.ping(message)
-    end
+    #end
   end
 
   def destroy
     @attend = Attend.find(params[:id])
-    if Rails.env == 'production'
+    #if Rails.env == 'production'
       notifier = Slack::Notifier.new(Rails.application.config.slack_webhook_url)
       @numbers = @attend.event.attends.size - 1 
       message = @attend.user.user_name.to_s + "さんが「" + @attend.event.name + "」の参加をキャンセルしました。現在のパーティ参加者は" + @numbers.to_s + "人です。"
       notifier.ping(message)
-    end
+    #end
     @attend.destroy
     redirect_to root_url
   end
