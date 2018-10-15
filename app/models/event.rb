f976dd066ffbc8e3b43a3c31ae363afaf22a91a4
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-10-15 13:39:39 +0900

    イベントの文字数を長くした

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   validates :name, length: {maximum: 50}
-  validates :context, length: {maximum: 240}
+  validates :context, length: {maximum: 1300}
+  validates :place, length: {maximum: 140}
+  validates :contact, length: {maximum: 140}
   belongs_to :user
   has_many :attends, :dependent => :delete_all
 end
