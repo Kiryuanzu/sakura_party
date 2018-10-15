dae8af62e8b9fe55f0d3ee2391fc9019ba3b6839
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-10-15 14:17:40 +0900

    タイトルの文字数を変えた

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
-  validates :name, length: {maximum: 50}
+  validates :name, length: {maximum: 100}
   validates :context, length: {maximum: 1300}
-  validates :place, length: {maximum: 140}
-  validates :contact, length: {maximum: 140}
+  validates :place, length: {maximum: 240}
+  validates :contact, length: {maximum: 240}
   belongs_to :user
   has_many :attends, :dependent => :delete_all
 end
