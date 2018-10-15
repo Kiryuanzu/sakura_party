896882569d9d8fc1d2ad49cde589fa1b2e3a132d
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-10-15 15:29:02 +0900

    文字数修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-  validates :name, length: {maximum: 100}
-  validates :context, length: {maximum: 1300}
+  validates :name, length: {maximum: 140}
+  validates :context, length: {maximum: 2000}
   validates :place, length: {maximum: 240}
   validates :contact, length: {maximum: 240}
   belongs_to :user
