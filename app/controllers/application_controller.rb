634e536ae613f67c6f1595f87f4317a93fd4fa1a
kiryuanzu <anzu.nagato.1005@gmail.com>
2019-02-17 16:57:07 +0900

    staging環境にBasic認証をかけた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
+  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "staging"
 
   private
 
